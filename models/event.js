3102e5ed4e4b1d12dae444ca9921c91fcd5f892b
CortezT <trysyanmc22@gmail.com>
2023-06-26 21:09:08 -0500

    added data types to post model

--- models/event.js
+++ models/event.js
@@ -21,11 +21,42 @@ Post.init(
             type: DataTypes.TEXT,
             allowNull: false
         },
+        //Added an event date to display the date of an event
+        event_date: {
+            type: DataTypes.DATE,
+            allowNull: false,
+        },
+        //Adds event location data type
+        eventLocation: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        //Adds event location data type using coordinates
+        locationCoordinates: {
+            Type: DataType.STRING,
+            allowNull: false,
+        },
+
+        isPrivate: {
+            //Define a boolean attribute
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: false,
+        },
+
+        updatedEvent: {
+            //Stores the timestamp of when a record was last updated
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        },
+
         date_created: {
             type: DataTypes.DATE,
             allowNull: false,
             defaultValue: DataTypes.NOW,
         },
+
         user_id: {
             type: DataTypes.INTEGER,
             references: {
@@ -44,4 +75,3 @@ Post.init(
 );
 
 module.exports = Post;
-    
