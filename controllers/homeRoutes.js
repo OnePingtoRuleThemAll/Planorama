8888494c32a40aafaa8910d6905d456a33169470
Jeffrey Vicente <administrator@Jeffreys-MacBook-Air.local>
2023-06-29 19:09:44 -0700

    Finished all of controllers and made changes to auth.js

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,32 +2,86 @@ const router = require('express').Router();
 const { Event, User }= require('../models');
 const withAuth = require('../utils/auth');
 
-//Get all the projects and JOIN with user data
+//Get all the events and JOIN with user data
 router.get('/', async (req, res) => {
     try{
+        // Get all events and JOIN with user
+        const eventData = await Event.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['username']
+                },
+            ],
+        });
 
+        // Serialize data so the template can read it
+        const events = eventData.map((event) => event.get({plain: true}));
+
+        // Pass serialized data and session flag into template
+        res.render('homepage', {
+            events,
+            logged_in: req.session.logged_in
+        });
     } catch(err) {
-
+        res.status(500).json(err);
     }
 
 });
 
-//Gets a single project depending on the ID
-router.get('/project/:id', async (req,res) => {
+//Gets a single event depending on the ID
+router.get('/event/:id', async (req,res) => {
     try{
+        const eventData = await Event.findByPk(req.params.id, {
+            include: [
+                {
+                    mode: User,
+                    attributes: ['username'],
+                },
+            ],
+        });
 
+        const event = eventData.get({plain: true});
+
+        res.render('event', {
+            ...event,
+            logged_in: req.session.logged_in
+        });
     } catch(err) {
-
+        res.status(500).json(err);
     }
 });
 
-//Route to access user profile
-router.get('profile' ,withAuth, async (req, res) =>{
+// Use withAuth middleware to prevent access to route
+router.get('/profile' ,withAuth, async (req, res) =>{
     try{
+        // Find the logged in user based on the session ID
+        const userData = await User.findByPk(req.session.user_id, {
+            attributes: { exclude: ['password']},
+            include: [{model:Event}],
+        });
 
+        const user = userData.get({ plain: true});
+
+        res.render('profile', {
+            ...user,
+            logged_in: true
+        });
     } catch(err){ 
-
+        res.status(500).json(err);
     }
 
 });
 
+router.get('/login', (req,res) => {
+    // If the user is already logged in, redirect the request to another route
+    if (req.session.logged_in) {
+        res.redirect('/profile');
+        return;
+    }
+
+    res.render('login');
+
+});
+
+module.exports = router;
