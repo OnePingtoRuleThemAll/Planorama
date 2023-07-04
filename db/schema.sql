d6cd83e39ece37f94027fb7ec31d63989bfcb2f0
OnePingtoRuleThemAll <126300119+OnePingtoRuleThemAll@users.noreply.github.com>
2023-07-03 19:11:42 -0500

    Update schema.sql
    added everything to the table for newevents

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,16 @@
 DROP DATABASE IF EXISTS planorama_db;
 
 CREATE DATABASE planorama_db;
+
+USE planorama_db;
+
+CREATE TABLE newEvent (
+    id INT NOT NULL,
+    event_title VARCHAR(40) NOT NULL,
+    event_body VARCHAR(150) TEXT NOT NULL,
+    event_location VARCHAR(30) TEXT NOT NULL,
+    event_coordinates VARCHAR(30) TEXT NOT NULL,
+    event_privacy VARCHAR(10) TEXT NOT NULL,
+    event_date VARCHAR(20) TEXT NOT NULL,
+    event_createdate VARCHAR(20) TEXT NOT NULL
+  )
