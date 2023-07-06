b6c9cbee4f3a5930f2e41e68a726971d955fce91
CortezT <trysyanmc22@gmail.com>
2023-07-05 19:35:37 -0500

    added users to event in seeds.js

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,32 +1,41 @@
 const sequelize = require('../config/connection');
 const { User, Event } = require('../models');
 
-
 const userData = require('./userData.json');
 const eventData = require('./eventData.json');
 
 const seedDatabase = async () => {
+  // Sync the database, dropping and recreating all tables
   await sequelize.sync({ force: true });
 
+  // Bulk create user records
   const users = await User.bulkCreate(userData, {
     individualHooks: true,
     returning: true,
   });
 
-  const event = await Event.bulkCreate(eventData, {
+  // Bulk create event records
+  const events = await Event.bulkCreate(eventData, {
     individualHooks: true,
     returning: true,
   });
 
-  /*
-  for (const project of projectData) {
-    await Project.create({
-      ...project,
-      user_id: users[Math.floor(Math.random() * users.length)].id,
-    });
+  // Associate users with events
+  for (const event of events) {
+    // Find associated user IDs for the current event
+    const associatedUserIds = eventData.find((e) => e.name === event.name)?.users;
+    if (associatedUserIds && associatedUserIds.length > 0) {
+      // Filter users to include only those with associated IDs
+      const usersToAdd = users.filter((user) => associatedUserIds.includes(user.id));
+
+      // Associate the filtered users with the current event
+      await event.addUsers(usersToAdd);
+    }
   }
-  */
+
+  // Exit the script
   process.exit(0);
 };
 
+// Call the seedDatabase function
 seedDatabase();
